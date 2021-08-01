5f459773d4808b75d2578fe3cb5b2e0dff029257
Alex Boehm <alexboehm@systemli.org>
2021-08-01 19:40:31 +0930

    yeet

---
+++ ex02/test.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+#include <stdio.h>
+
+int	main(void)
+{
+
+{
+    int number=60;  
+    char charValue=number+'0';
+    printf("The value is :%c",charValue);
+    return 0;
+}
+
+}
