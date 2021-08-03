48a1186d5ae1203ddeb2da5108e0874a0015a122
Alex Boehm <alexboehm@systemli.org>
2021-08-03 12:34:05 +0930

    commited

--- ex02/ft_putnmbr.c
+++ ex02/ft_putnmbr.c
@@ -1,4 +1,5 @@
 #include <unistd.h>
+
 void	ft_putchar(char c)
 {
 	write(1, &c, 1);
@@ -16,14 +17,13 @@ void	ft_putnbr(int nb)
 		ft_putchar('-');
 		nb = -nb;
 	}
-	if (nb <= 9)
+	if (nb > 9)
 	{
-		ft_putchar(nb + '0');
-		return ;
+		ft_putnbr(nb / 10);
+		ft_putnbr(nb % 10);
 	}
-	ft_putnbr(nb / 10);
-	ft_putchar((nb % 10) + '0');
-	return ;
+	else
+		ft_putchar(nb + 48);
 }
 /*
 int	main(void)
